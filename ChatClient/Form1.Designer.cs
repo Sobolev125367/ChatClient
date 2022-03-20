a200cbcf5d99b7c650f52691300474e75b8b9b64
Oleg <Oleg@DESKTOP-419AEEE>
2022-03-20 19:34:52 +0300

    Добавьте файлы проекта.

---
+++ ChatClient/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace ChatClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.менюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.авторToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.Location = new System.Drawing.Point(696, 40);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 56);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Sign in";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textBox1.Location = new System.Drawing.Point(12, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(158, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(168, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Chat:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(77, 544);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Type a message:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(171, 102);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(617, 424);
+            this.richTextBox1.TabIndex = 6;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.richTextBox2.Location = new System.Drawing.Point(171, 532);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(536, 35);
+            this.richTextBox2.TabIndex = 7;
+            this.richTextBox2.Text = "";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.менюToolStripMenuItem,
+            this.авторToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // менюToolStripMenuItem
+            // 
+            this.менюToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.settingsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.менюToolStripMenuItem.Name = "менюToolStripMenuItem";
+            this.менюToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.менюToolStripMenuItem.Text = "Меню";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            this.settingsToolStripMenuItem.Click += new System.EventHandler(this.settingsToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // авторToolStripMenuItem
+            // 
+            this.авторToolStripMenuItem.Name = "авторToolStripMenuItem";
+            this.авторToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.авторToolStripMenuItem.Text = "Автор";
+            this.авторToolStripMenuItem.Click += new System.EventHandler(this.авторToolStripMenuItem_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(398, 62);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::ChatClient.Properties.Resources.sen;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Location = new System.Drawing.Point(713, 532);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 37);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 589);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Chat";
+            this.TransparencyKey = System.Drawing.Color.Black;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem менюToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem авторToolStripMenuItem;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ ChatClient/Form1.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+namespace ChatClient
+{
+    public partial class Form1 : Form
+    {
+        private string placeholder = "Введите ник";
+
+        //Сокет для получения сообщения
+        static private Socket Client;
+        private IPAddress ip = null;
+        private int port = 0;
+        private Thread thread;
+        public Form1()
+        {
+            InitializeComponent();
+
+            richTextBox1.Enabled = false;
+            richTextBox2.Enabled = false;
+            button1.Enabled = false;
+
+            //Создаем обработчик ошибок
+            try
+            {
+                var sr = new StreamReader(@"Client_info/data_info.txt");
+                //Переменную для хранения данных из файла
+                string buffer = sr.ReadToEnd();
+                sr.Close();
+                //Делим/парсим инфу
+                string[] connect_info = buffer.Split(':');
+                ip = IPAddress.Parse(connect_info[0]);
+                port = int.Parse(connect_info[1]);
+
+                label4.ForeColor = Color.Blue;
+                label4.Text = "Настройки:\n IP сервера:"+connect_info[0]+"\n Порт сервера:" + connect_info[1];
+            }
+            catch(Exception ex)
+            {
+                label4.ForeColor=Color.Red;
+                label4.Text="Настройки не найдены!";
+                Settings settings = new Settings();
+                settings.Show(); 
+            }
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.ForeColor = Color.Gray;
+            textBox1.Text = placeholder;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SendMessage("\n" + textBox1.Text + ": " + richTextBox2.Text + ";;;5");
+            richTextBox2.Clear();
+        }
+
+        private void settingsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Settings settings = new Settings();
+            settings.Show();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //Остановка метода thread
+            if(thread!= null)
+                thread.Abort();
+            if(Client!=null)
+            {
+                Client.Close();
+            }    
+            Application.Exit();
+        }
+        //Получение сообщения от сервера
+        void SendMessage(string message)
+        {
+            if(message !="" && message!="")
+            {
+                byte[] buffer = new byte[1024];
+                buffer=Encoding.UTF8.GetBytes(message);
+                Client.Send(buffer);
+            }
+        }
+
+        void RecvMessage()
+        {
+            byte[] buffer = new byte[1024];
+            //Чистим буфер
+            for(int i=0;i<buffer.Length;i++)
+            {
+                buffer[i] = 0;
+            }
+            for(; ; )
+            {
+                try
+                {
+                    Client.Receive(buffer);
+                    //Переводим полученное сообщение
+                    string Message=Encoding.UTF8.GetString(buffer);
+                    //Конец буфера
+                    int count = Message.IndexOf(";;;5");
+                    if(count==-1)
+                    {
+                        continue;
+                    }
+                    string ClearMessage = "";
+                    for(int i=0; i<count;i++)
+                    {
+                        ClearMessage += Message[i];
+                    }
+                    for (int i = 0; i < buffer.Length; i++)
+                    {
+                        buffer[i] = 0;
+                    }
+                    this.Invoke((MethodInvoker)delegate ()
+                    {
+                        richTextBox1.AppendText(ClearMessage);
+                    });
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text !=" "&& textBox1.Text != "")
+            {
+                button1.Enabled = true;
+                richTextBox2.Enabled = true;
+                Client=new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                {
+                    //Создаем запрос
+                    if (ip != null)
+                    {
+                        //Подключаемся к серверу
+                        Client.Connect(ip, port);
+                        //Запуск метода для получения сообщений от сервера
+                        thread = new Thread(delegate () { RecvMessage(); });
+                        thread.Start();
+                        richTextBox2.Focus();
+                    }
+                }
+
+            }
+
+        }
+
+        private void авторToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("See my profile on github: github.com/Sobolev125367");
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                SendMessage("\n" + textBox1.Text + ": " + richTextBox2.Text + ";;;5");
+                richTextBox2.Clear();
+            }
+        }
+        //Пишем фокус для первого текстбокса при входе в него
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+            if (textBox1.Text == placeholder)
+            {
+                textBox1.Text = "";
+                textBox1.ForeColor = Color.Black;
+            }
+        }
+        //Пишем фокус для первого текстбокса при выходе из него
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+            if(textBox1.Text == "")
+            {
+                textBox1.ForeColor= Color.Gray;
+                textBox1.Text = placeholder;
+            }
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+
+
+
+        /*
+                //Пишем фокус для второго текстбокса при входе в него
+                private void richTextBox2_Enter_1(object sender, EventArgs e)
+                {
+                    if (richTextBox2.Text == placeholder)
+                    {
+                        richTextBox2.Text = "";
+                        richTextBox2.ForeColor = Color.Black;
+                    }
+                }
+                //Пишем фокус для второго текстбокса при выходе из него
+                private void richTextBox2_Leave_1(object sender, EventArgs e)
+                {
+                    if (richTextBox2.Text == "")
+                    {
+                        richTextBox2.ForeColor = Color.Gray;
+                        richTextBox2.Text = placeholder;
+                    }
+                }
+        */
+    }
+}
